94c748f4b1c7a2af98f76aa144b0c8aa92b2b112
Allen Chang <iafayte@gmail.com>
2015-06-25 11:28:45 -0500

    Songmodel tests passed & app/appview

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,6 +4,7 @@ var AppView = Backbone.View.extend({
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.queueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
@@ -16,6 +17,7 @@ var AppView = Backbone.View.extend({
     return this.$el.html([
       this.playerView.$el,
       this.libraryView.$el
+      // this.queueView.$el
     ]);
   }
 
